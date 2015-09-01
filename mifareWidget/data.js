1060f626d5b56d07777567f7dcfa11a122d7b7f4
Karl Backström <backstrom@akafugu.jp>
2015-09-01 15:16:32 +0900

    Typo

--- mifareWidget/data.js
+++ mifareWidget/data.js
@@ -25,7 +25,7 @@
        		["1", "0", "1", "key B<sup>1</sup>", "never", "never", "never", "read/write block"],
        		["1", "1", "1", "never", "never", "never", "never", "read/write block"]
        		],
-       		"note" : "<sup>1</sup> if Key B may be read in the corresponding Sector Trailer it cannot serve for authentication (all grey marked lines in previous table). As a consequences, if the reader authenticates any block of a sector which uses the grey marked access conditions and using key B, the card will refuse any subsequent memory access after authentication."
+       		"note" : "<sup>1</sup> if Key B may be read in the corresponding Sector Trailer it cannot serve for authentication (all grey marked lines in last table). As a consequences, if the reader authenticates any block of a sector which uses the grey marked access conditions and using key B, the card will refuse any subsequent memory access after authentication."
    	   },
    	   "copyright" : "HTMLified by <a href=\"http://akafugu.jp\">Akafugu Corporation</a>.<br/>The information is taken from <a href=\"http://www.nxp.com/documents/data_sheet/MF1S503x.pdf\">MF1S503x</a> from NXP Semiconductors." 
    }
